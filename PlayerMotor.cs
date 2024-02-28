f2d8c7facf3705c1274a8cfc1d500bf535bb045d
Jake <95217966+TahjJake@users.noreply.github.com>
2024-02-28 14:06:22 +0000

    Add files via upload

---
+++ PlayerMotor.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool isGrounded;
+    public float playerSpeed = 5f;
+    public float gravity = -9.8f;
+    public float jumpHeight = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = controller.isGrounded;
+    }
+    public void ProcessMove(Vector2 input)
+    {
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection.x = input.x;
+        moveDirection.z = input.y;
+        controller.Move(transform.TransformDirection(moveDirection) * Time.deltaTime * playerSpeed);
+        playerVelocity.y += gravity * Time.deltaTime;
+        if(isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -2f;
+        }
+        controller.Move(playerVelocity * Time.deltaTime);
+        Debug.Log(playerVelocity.y);
+    }
+    public void Jump()
+    {
+        playerVelocity.y = Mathf.Sqrt(jumpHeight * -3f * gravity);
+    }
+}
